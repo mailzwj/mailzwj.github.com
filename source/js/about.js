7c79a4c892a10a9851b52eaa62e0939d5f7cbdb1
Mr.Zheng <mailzwj@126.com>
2021-01-02 17:21:04 +0800

    优化关于我页面

--- source/js/about.js
+++ source/js/about.js
@@ -1,17 +1,34 @@
 (function() {
     var box = document.querySelector('#J_NjvAbout');
     var repoApi = 'https://api.github.com/users/mailzwj/repos';
+    var myRepos = [];
+    var transPrefix = '';
+
+    if ('webkitTransform' in box.style) {
+        transPrefix = '-webkit-';
+    } else if ('mozTransform' in box.style) {
+        transPrefix = '-moz-';
+    } else if ('msTransform' in box.style) {
+        transPrefix = '-ms-';
+    }
 
     function renderRepos(data) {
+        var boxWidth = box.offsetWidth;
+        var winWidth = window.innerWidth;
+        var itemWidth = boxWidth * 0.24;
         var repos = data || [];
         var len = repos.length;
         if ('HTMLPortalElement' in window) {
-        // if (false) {
             for (var x = 0; x < len; x++) {
+                var div = document.createElement('div');
                 var item = document.createElement('portal');
-                item.className = 'njv-portal';
+                var ct = `width: ${winWidth}px;height: ${(winWidth / itemWidth) * 108}px;`;
+                ct += `${transPrefix}transform-origin: left top;${transPrefix}transform: scale(${itemWidth / winWidth});`;
+                div.className = 'njv-portal';
                 item.src = `//newjs.vip/${repos[x].name}`;
-                box.appendChild(item);
+                item.style.cssText = ct;
+                div.appendChild(item);
+                box.appendChild(div);
             }
         } else {
             for(var x = 0; x < len; x++) {
@@ -29,6 +46,14 @@
     axios.get(repoApi).then(function(res) {
         return res.data || [];
     }).then(function(data) {
+        myRepos = data;
         renderRepos(data);
     });
+
+    window.addEventListener('reset', () => {
+        if (myRepos.length) {
+            box.innerHTML = '';
+            renderRepos(myRepos);
+        }
+    }, false);
 })();
